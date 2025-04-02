c6b4ca91eaad92e51524bbf73fc02ba60cb466fd
Nikhil Patel <68939870+naive-nikhil@users.noreply.github.com>
2025-04-03 01:00:02 +0530

    Some Changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
-const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/fakefriends';
+require("dotenv").config();
+const mongoose = require("mongoose");
+const DB_URI = process.env.MONGODB_URL;
 
-const connectDB = async ()=>{
-    try {
-        await mongoose.connect(DB_URI);
-        console.log(`MongoDB Connected: ${mongoose.connection.host}`);
-    } catch (error) {
-        console.error(`Error Connecting to Database: ${error.message}`);
-        process.exit(1);
-        
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB_URI);
+    console.log(`MongoDB Connected: ${mongoose.connection.host}`);
+  } catch (error) {
+    console.error(`Error Connecting to Database: ${error.message}`);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
